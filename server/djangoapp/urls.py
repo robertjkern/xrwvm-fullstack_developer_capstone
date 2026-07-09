ac6cef836955b2c5277a3603f3433c6297049627
robertjkern <robert.kern@amway.com>
2026-07-09 13:20:49 -0400

    frontend build

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -3,13 +3,16 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
+from django.views.generic import TemplateView
 
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
 
     # path for login
+    path('login/', TemplateView.as_view(template_name="index.html")),
     path(route='login', view=views.login_user, name='login'),
+    
     
 
     # path for dealer reviews view
