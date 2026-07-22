ffb88822c75b06e4417b1859aa607f8caf67ddcc
robertjkern <robert.kern@amway.com>
2026-07-22 12:54:48 -0400

    changes to get all cars

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -18,5 +18,9 @@ urlpatterns = [
     
     # path for dealer reviews view
 
+    # path for get_cars
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
+
+
     # path for add a review view
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
